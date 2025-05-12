bb8507da48a4e2e0ec422819ed8120713a55d816
FarzanaTN <nishatfarzana14@gmail.com>
2025-05-12 16:49:06 +0600

    hello

--- mid/27.s
+++ mid/27.s
@@ -16,11 +16,13 @@ main
         MOV     R4, #0x44dd
 
         ; Load address of MyData into R5
-        LDR     R5, =MyData
+        LDR     R5, =MyData; 0x20000000
 
         ; Perform the STR operations
-        STR     R2, [R5, #4]      ; Store R2 at R5 + 4
-        STR     R3, [R5], #4      ; Store R3 at R5, then R5 = R5 + 4
+        STR     R2, [R5, #4]      ; Store R2 at R5 + 4, 0x20000004 is bb and 0x20000005 is 22
+        LDR     R5, =MyData; 0x20000000
+        STR     R3, [R5], #4      ; Store R3 at R5, then R5 = R5 + 4, 0x20000000 is cc and 0x20000001 is 33
+        ;now r5 is 0x20000004
 
 stop
         B       stop             ; Infinite loop to halt program
